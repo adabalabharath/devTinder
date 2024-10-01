f2b7030e862060796248d110af52eec746f97c49
adabalabharath <saibharath.adabala@gmail.com>
2024-10-01 23:31:24 +0530

    database init

--- src/app.js
+++ src/app.js
@@ -1,26 +1,33 @@
 let express = require("express");
-let adminAuth = require("../auth");
+const connectDB = require("./config/database");
+const User = require("./model/Schema");
+
 let app = express();
 
-
-app.get("/getData", adminAuth, (req, res, next) => {
-    //use try catch
-  throw new Error("error at fetching");
-  res.send("data fetched successfully");
-});
-
-
-app.get("/hi", (req, res, next) => {
-  res.send("hi-route");
-});
-
-app.use("/", (err, req, res, next) => {
-  console.log("default-route");
-  if (err) {
-    res.status(500).send("something wrong");
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
+  const userDetails = {
+    firstName: "msd",
+    lastName: "ms",
+    email: "msd@gmail.com",
+    password: "msd@123",
+    age: 30,
+    gender: "male",
+  };
+  let user = new User(userDetails);
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (error) {
+    res.status(404).send("unsuccessful");
   }
 });
 
-app.listen(7777, () => {
-  console.log("listening port 7777");
-});
+connectDB()
+  .then(() => {
+    console.log("db established successfully");
+    app.listen(7777, () => {
+      console.log("listening port 7777");
+    });
+  })
+  .catch(() => console.error("db error"));
---
+++ src/model/Schema.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  email: { type: String },
+  password: { type: String },
+  age: { type: Number },
+  gender: { type: String },
+});
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
