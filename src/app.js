3a8279a2e6812c4d8a22870b24531ec29b195657
adabalabharath <saibharath.adabala@gmail.com>
2024-10-01 12:18:04 +0530

    wildcard error handling

--- src/app.js
+++ src/app.js
@@ -2,19 +2,23 @@ let express = require("express");
 let adminAuth = require("../auth");
 let app = express();
 
+
+app.get("/getData", adminAuth, (req, res, next) => {
+    //use try catch
+  throw new Error("error at fetching");
+  res.send("data fetched successfully");
+});
+
+
+app.get("/hi", (req, res, next) => {
+  res.send("hi-route");
+});
+
 app.use("/", (err, req, res, next) => {
   console.log("default-route");
   if (err) {
     res.status(500).send("something wrong");
   }
-});
-app.get("/getData", adminAuth, (req, res, next) => {
-  throw new Error("error at fetching");
-  res.send("data fetched successfully");
-});
-
-app.get("/hi", (req, res, next) => {
-  res.send("hi-route");
 });
 
 app.listen(7777, () => {
