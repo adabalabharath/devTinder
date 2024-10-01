ece711b7581e13a6fd6f849741aa6389bf49bd9d
adabalabharath <saibharath.adabala@gmail.com>
2024-10-01 12:15:13 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ let express = require("express");
 let adminAuth = require("../auth");
 let app = express();
 
-app.use("/", (req, res, next) => {
+app.use("/", (err, req, res, next) => {
   console.log("default-route");
-  next();
+  if (err) {
+    res.status(500).send("something wrong");
+  }
 });
 app.get("/getData", adminAuth, (req, res, next) => {
+  throw new Error("error at fetching");
   res.send("data fetched successfully");
 });
 
